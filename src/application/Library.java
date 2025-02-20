6f13d30599016a87b73b71c9a3ded5291667e532
AnsonL1234 <ansontan1001@gmail.com>
2025-02-20 13:17:16 +0000

    the part 3

--- src/application/Library.java
+++ src/application/Library.java
@@ -1,58 +1,22 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Library {
 
-    private ArrayList<Book> booksList = null;
+    /** 1.1 A instance variable that will store in the collection - In this case, I will use ArrayList */
+    private List<Book> booksList = null;
 
-    public Library() {
-        booksList = new ArrayList<>();
-        Book[] book = new Book[3];
-        Scanner scan = new Scanner(System.in);
-        String isbn = "";
-
-        book[0] = Book.builder().authorFirstName("Anson").authorSurname("Ling").isbn("AB1111").publishYear(2001).build();
-        book[1] = Book.builder().authorFirstName("Sebastian").authorSurname("Moyo").isbn("AB1122").publishYear(2001).build();
-        book[2] = Book.builder().authorFirstName("Khalid").authorSurname("Slim").isbn("AB1133").publishYear(2001).build();
-        booksList.add(book[0]);
-        booksList.add(book[1]);
-        booksList.add(book[2]);
-
-        System.out.println("Please enter the isbn: ");
-        isbn = scan.nextLine();
-
-        int index = findIndexBook(isbn);
-        System.out.println(index);
-
-        try {
-            //holding the current book that wish to loan
-            Book book1 = loanBook(index);
-            System.out.println("The loan book is " + loanBook(index));
-
-            //remove the loan book from the collection
-            booksList.remove(loanBook(index));
-
-            //display the book list to check
-            System.out.println("Book Detail: \n");
-            for (Book book2: booksList)
-                System.out.println(book2);
-
-            //return the loan book
-            returnBook(book1);
-
-            //display the info
-            System.out.println("Book Detail: \n");
-            for (Book book2: booksList)
-                System.out.println(book2);
-
-            System.out.println("The book has been return!");
-        } catch (Exception e) {
-            System.out.println("The loan book is not found in the collection!");
-        }
+    public Library(Book book) {
+        booksList = new ArrayList<Book>();
+        this.booksList.add(book);
     }
 
+    /**
+     * 1.2 function method that find the book by index
+     */
     public int findIndexBook(String isbn) {
         for(int i = 0; i < booksList.size(); i++) {
             if(booksList.get(i).isbn().equals(isbn))
@@ -62,6 +26,7 @@ public class Library {
     }
 
     /**
+     * 1.3
      * This is the object function method that return the loan book value
      * return if the book is found in the location
      * return null otherwise
@@ -77,11 +42,31 @@ public class Library {
         return null;
     }
 
+    /**
+     * 1.4 pure method for return the book by the Book object parameters
+     */
     public void returnBook(Book book) {
         booksList.add(book);
     }
 
+    /**
+     * 1.5 Create library object that will store the hardcore value and create two member object that run both thread
+     */
     public static void main(String[] args) {
-        new Library();
+        Library myLibrary = null;
+        Book book = Book.builder()
+                .authorFirstName("Guang Cheng")
+                .authorSurname("Anson Ling")
+                .isbn("AB11228")
+                .publishYear(2001)
+                .build();
+
+        myLibrary = new Library(book);
+
+        Member member1 = new Member(myLibrary);
+        Member member2 = new Member(myLibrary);
+        member1.start();
+        member2.start();
+
     }
 }
--- src/application/Member.java
+++ src/application/Member.java
@@ -4,5 +4,126 @@ import java.util.Scanner;
 
 public class Member extends Thread{
 
+    private Library library = null;
+    private String isbn = "AB11228";
 
+    public Member(Library library) {
+        this.library = library;
+    }
+//System.out.println();
+    public void run() {
+        /** 3.A. Call the findIndexBook */
+        int index = library.findIndexBook(this.isbn);
+
+        Book book = null;
+
+        //check to see is the book found
+        if (index < 0) {
+            System.out.println("The book you wish to find is not found!");
+        } else {
+
+            /** 3.B. Call the loan book */
+            book = library.loanBook(index);
+
+            /** 3.C. Sleep for 2000 millis and call return book */
+            try {
+                Thread.sleep(2000);
+                System.out.println("The book you wish to find is found!");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            library.returnBook(book);
+            System.out.println("The book you loan is return!");
+        }
+    }
+}
+
+class Repository {
+    private int integer;
+    private boolean isNotify = false;
+
+    public Repository() {
+        this.integer = 0;
+    }
+
+    synchronized int getInteger() {
+        return this.integer;
+    }
+
+    synchronized void setInteger(int n) {
+        this.integer = n;
+    }
+
+    synchronized boolean isNotify() {
+        return this.isNotify;
+    }
+
+    synchronized void setIsNotify(boolean isNotify) {
+        this.isNotify = isNotify;
+    }
+
+    public String toString() {
+        return "The number: " + this.integer;
+    }
+}
+
+class Counter extends Thread{
+    Repository repository;
+
+    public Counter(Repository repository) {
+        this.repository = repository;
+    }
+
+    public void run() {
+        synchronized (this) {
+            try {
+                repository.wait();
+                r
+//                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            for (int i = 1; i <= 5; i++) {
+                repository.setInteger(i);
+                System.out.println("Continuous count " + i);
+            }
+        }
+    }
+}
+
+class Publish extends Thread {
+    Repository repository;
+
+    public Publish(Repository repository) {
+        this.repository = repository;
+    }
+
+    public void run() {
+        for (int i = 1; i <= repository.getInteger(); i++)
+            System.out.println("Print table " + i);
+
+        repository.notify();
+
+
+//                try {
+//
+//    //             Thread.sleep(1000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+
+    }
+}
+
+class Program {
+
+    public static void main(String[] args) {
+        Repository repository = new Repository();
+        Counter counter = new Counter(repository);
+        Publish publish = new Publish(repository);
+        counter.start();
+        publish.start();
+    }
 }
